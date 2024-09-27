f337a414d42460341fdd5cd71fa5e0ba1930e357
Matthias <matthias.poortvliet@kpn.com>
2024-09-27 21:05:48 +0200

    add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  }
 })
