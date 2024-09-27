539c4f6be765b49f942cc618d593dbf4977335e6
Matthias <matthias.poortvliet@kpn.com>
2024-09-27 08:11:10 +0200

    added timezone

--- server/scraping/runDelayedFlow.ts
+++ server/scraping/runDelayedFlow.ts
@@ -1,4 +1,5 @@
 import dayjs, { Dayjs } from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
 import type { Flow, Action } from '@/types/flow'
 import { runFlow } from '@/server/scraping/runFlow';
 import { job, setJob, stopJob, setJobStatus } from '@/server/cron/job.js'
@@ -8,10 +9,13 @@ export const runDelayedFlow = async (
     payload : Record<string, Action>,
     bookingThreshold: number
 ) => {
+    dayjs.extend(timezone);
+    dayjs.tz.setDefault('Europe/Amsterdam');
+
     const { value : bookingDate } = payload.dateSelect;
     const { value : timeCourtSelect } = payload.timeCourtSelect;
 
-    const jobStartDate = dayjs(bookingDate as string).subtract(bookingThreshold, 'day');
+    const jobStartDate = dayjs(bookingDate as string).subtract(bookingThreshold - 1, 'day')
     const [time, court] = timeCourtSelect as string[];
     
     const message = `flow will run at ${jobStartDate.toISOString()} at ${payload.dateSelect.value} : ${time} on court ${court}`;
