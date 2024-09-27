9bad5c895729640089b913a0fb7d1d9f088f68f8
Matthias <matthias.poortvliet@kpn.com>
2024-09-27 15:23:36 +0200

    updated import path for utc

--- server/scraping/runDelayedFlow.ts
+++ server/scraping/runDelayedFlow.ts
@@ -1,5 +1,5 @@
 import dayjs, { Dayjs } from 'dayjs';
-import utc from 'dayjs/plugin/utc'
+import utc from 'dayjs/plugin/utc.js'
 import timezone from 'dayjs/plugin/timezone.js';
 import type { Flow, Action } from '@/types/flow'
 import { runFlow } from '@/server/scraping/runFlow';
