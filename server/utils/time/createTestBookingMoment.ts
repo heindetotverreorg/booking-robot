0068a4e80a273310af7177f8417e73460a34c2c6
Matthias <matthias.poortvliet@kpn.com>
2024-12-15 21:34:15 +0100

    add logs

--- server/utils/time/createTestBookingMoment.ts
+++ server/utils/time/createTestBookingMoment.ts
@@ -1,9 +1,15 @@
 import moment from 'moment-timezone';
 import { config } from '@/server/config';
 
-const timeZoneOffset = moment().tz('Europe/Amsterdam').utcOffset() / 60;
 
 export default  (bookingDate : string) => {
+    const timeZoneOffset = moment().tz('Europe/Amsterdam').utcOffset() / 60;
+    console.log('timeZoneOffset', timeZoneOffset)
+
     const [hours, minutes] = config.cronTestTime.split(':')
-    return moment().set({ hours: parseInt(hours) - timeZoneOffset, minutes: parseInt(minutes) })
+    return moment()
+    .set({
+        // hours: parseInt(hours) - timeZoneOffset,
+        minutes: parseInt(minutes)
+    })
 }
