c9b4aaf2ea9b58745383dcf3af840b82c28f7d79
Matthias <matthias.poortvliet@kpn.com>
2025-01-11 22:49:26 +0100

    destimezoned

--- server/utils/time/createJobStartMoment.ts
+++ server/utils/time/createJobStartMoment.ts
@@ -5,10 +5,12 @@ import utc from 'dayjs/plugin/utc.js';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+console.log('RUN WITH DETIMEZONING IN createJobStartMoment.ts')
+
 export default (bookingDate : string, bookingThreshold : number) => {
     return dayjs(bookingDate)
         .subtract(bookingThreshold, 'day')
         .set('hour', 0)
         .set('minute', 0)
-        .tz('Europe/Amsterdam')
+        // .tz('Europe/Amsterdam')
 }
