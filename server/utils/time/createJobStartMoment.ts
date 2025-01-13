fa79855cab13e9a8884fa03c3abaa18d76709113
Matthias <matthias.poortvliet@kpn.com>
2025-01-13 08:15:01 +0100

    added all sorts of logging for timezones

--- server/utils/time/createJobStartMoment.ts
+++ server/utils/time/createJobStartMoment.ts
@@ -9,6 +9,8 @@ function isDST(timestamp: any) {
     const amsterdamTime = dayjs.tz(timestamp, "Europe/Amsterdam")
     const utcTime = amsterdamTime.utc()
     const differenceInHours = amsterdamTime.diff(utcTime, 'hour');
+
+    console.log('differenceInHours', differenceInHours)
      
     return differenceInHours !== 1
 }
@@ -16,11 +18,12 @@ function isDST(timestamp: any) {
 export default (bookingDate : string, bookingThreshold : number) => {
     if (isDST(dayjs().format('YYYY-MM-DD HH:mm:ss'))) {
         console.log('IS DAYLIGHT SAVING TIME')
+        console.log('delete the substract 1 hour on daylight saving, expecting it to run at 02:00 real time')
         return dayjs(bookingDate)
         .subtract(bookingThreshold, 'day')
         .set('hour', 0)
         .set('minute', 0)
-        .subtract(1, 'hour')
+        // .subtract(1, 'hour')
         .tz('Europe/Amsterdam')
     }
 
